cfb75673360e7753823a264d4d42fb1e5e8735a4
dklopstein <dklopstein@ucsd.edu>
2025-03-17 23:56:56 -0700

    added conclusion image

--- main.js
+++ main.js
@@ -96,6 +96,14 @@ document.addEventListener("DOMContentLoaded", function () {
         .attr("xlink:href", "./diabetes_img2.png")
         .attr("width", "100%")
         .attr("height", "100%");
+      
+      svg
+        .append("image")
+        .attr("class", "conclusion-image")
+        .style("opacity", 0)
+        .attr("xlink:href", "./punch_conclusion.jpg")
+        .attr("width", "100%")
+        .attr("height", "100%");
 
       // Set up scales
       const xScale = d3.scaleTime().range([margin.left, width - margin.right]);
@@ -617,14 +625,38 @@ document.addEventListener("DOMContentLoaded", function () {
       if (isScrollingDown) {
         removeCluster();
         createGlucoseSpike();
-      } else if (isScrollingUp) {
-        // const spikeChart = d3.select(".spike-chart");
-        // spikeChart
-        //   .transition()
-        //   .duration(500)
-        //   .style("opacity", 0)
-        //   .remove();
-        // createCluster();
+      }
+    } else if (stepIndex == 10) {
+      if (isScrollingUp) {
+        chart.svg
+          .select(".conclusion-image")
+          .transition()
+          .duration(500)
+          .style("opacity", 0);
+        createGlucoseSpike();
+      }
+    }
+
+     else if (stepIndex == 11) {
+      if (isScrollingUp) {
+        chart.svg
+          .select(".conclusion-image")
+          .transition()
+          .duration(500)
+          .style("opacity", 0);
+      } else if (isScrollingDown) {
+        const svg = d3.select("svg");
+        svg
+          .selectAll("g")
+          .transition()
+          .duration(500)
+          .style("opacity", 0)
+          .remove();
+        chart.svg
+          .select(".conclusion-image")
+          .transition()
+          .duration(500)
+          .style("opacity", 1);
       }
     }
   }
